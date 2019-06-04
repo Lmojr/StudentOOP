951933e02786cd60ffabbc556cada7fa08f279c3
unknown <Lmo@LAPTOP-QSIT2ERO.NYU.EDU>
2019-06-04 13:08:00 -0400

    Create pointers.cpp

---
+++ my_code/pointers.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+	++n;
+}
+
+
+void inc_with_pointer(int* iptr) {
+	++(*iptr);
+}
+
+
+void inc_with_reference(int& n) {
+	++n;
+}
+
+
+Thing** create_array_of_things(int n) {
+	Thing** array = new Thing*[n];
+	for (int i = 0; i < n; i++) {
+		array[i] = new Thing(i);
+	}
+	return array;
+}
+
+void print_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << things[i]->val << ' ';
+	}
+	cout << endl;
+}
+
+void double_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		things[i]->val *= 2;
+	}
+}
+
+void delete_all_things(Thing** things, int n) {
+	for (int i = 0; i < n; i++) {
+		delete things[i];
+	}
+	delete [] things;
+}
+
+
+void assignTA(Student* s, Student* ta) {
+	s->ta = ta;
+}
+
+void printTAs(vector<Student*>& students) {
+	for (Student* s: students) {
+		if (s->ta == nullptr) {
+			cout << s->name << " has no TA." << endl;
+		}
+		else {
+			cout << s->name << " has TA " << s->ta->name << '.' << endl;
+		}
+	}
+	cout << endl;
+}
