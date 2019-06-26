bdda77bf28b9e194ffe254f0caf6ba685b21be74
unknown <Lmo@LAPTOP-QSIT2ERO.NYU.EDU>
2019-06-26 13:02:31 -0400

    Update tvector.h

--- my_code/tvector.h
+++ my_code/tvector.h
@@ -12,7 +12,7 @@ class MyVec {
 public:
 	MyVec() : sz(0) {
 		capacity = DEF_CAPACITY;
-		data = new int[DEF_CAPACITY];
+		data = new T[DEF_CAPACITY];
 	}
 
 
@@ -27,7 +27,7 @@ public:
 		if (sz == capacity) {
 			// get new array of capacity * 2
 			capacity *= 2;
-			int* new_data = new int[capacity];
+			int* new_data = new T[capacity];
 
 			// copy over existing data
 			for (int i = 0; i < size(); i++) {
@@ -60,7 +60,7 @@ public:
 			delete[] data;
 			capacity = v2.capacity;
 			sz = v2.size();
-			data = new int[capacity];
+			data = new T[capacity];
 			for (int i = 0; i < size(); ++i) {
 				data[i] = v2.data[i];
 			}
@@ -86,7 +86,7 @@ private:
 	void copy(const MyVec<T>& v2) {
 		sz = v2.sz;
 		capacity = v2.capacity;
-		data = new int[capacity];
+		data = new T[capacity];
 		for (int i = 0; i < sz; i++) {
 			data[i] = v2.data[i];
 		}
